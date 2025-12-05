e55442bef3a2deb905c341781e97928e50699138
kk1701 <karankute05@gmail.com>
2025-12-05 21:44:47 +0530

    added generic button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
+import { Button } from './components/ui/Button'
+import { PlusIcon } from './icons/PlusIcon'
+import { ShareIcon } from './icons/ShareIcon'
 
 function App() {
 
   return (
     <>
-
+      <Button startIcon={PlusIcon({size: "md"})} variant='primary' text="Add Content" size='md' onClick={() => {}}></Button>
+      <Button startIcon={ShareIcon({size: "md"})} variant='secondary' text="Share" size='md' onClick={() => {}}></Button>
     </>
   )
 }
